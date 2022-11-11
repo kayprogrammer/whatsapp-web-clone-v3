be7a33b631225a294e0f8873cf8e9375df15c61a
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2022-11-12 00:15:51 +0100

    added sth

--- whatsappwebclonev3/api.py
+++ whatsappwebclonev3/api.py
@@ -3,7 +3,7 @@ from apps.accounts.views import router as accounts_router
 from apps.chat.views import router as chat_router
 from . renderers import MyCustomRenderer
 
-api = NinjaAPI(renderer=MyCustomRenderer())
+api = NinjaAPI(renderer=MyCustomRenderer(), csrf=True)
 
 api.add_router("/accounts/", accounts_router)
 api.add_router("/chat/", chat_router)
